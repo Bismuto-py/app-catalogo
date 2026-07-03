14d9a6abaffd7649db87a8e810b0ef29929e2d41
Julia Hevellyn de Melo Fidelis <20261016270013@ifrn.local>
2026-07-02 21:54:02 -0300

    Modificando models

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Obra (models.Model):
+    TIPO_CHOICE = [
+        ("Filme", "Filme"),
+        ("Série", "Série"),
+    ]
+
+    titulo = models.CharField(max_length=150)
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICE)
+    ano = models.IntegerField()
+    genero = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py createsuperuser
+#poe o nome, email e senha, depois entra no admin do django e adiciona algumas obras
+#python manage.py runserver
